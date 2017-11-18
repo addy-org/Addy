da6119eb1ae85cd20374554b64bfdd566d6a21ef
Marcelo Cristiano <marceloabk@hotmail.com>
2017-11-18 16:16:42 -0200

    Adding webpack to gulp pipeline

--- src/gulp/tasks/production/optimize-js.js
+++ src/gulp/tasks/production/optimize-js.js
@@ -1,6 +1,7 @@
 var gulp   = require('gulp');
 var uglify = require('gulp-uglify');
 var size   = require('gulp-size');
+var webpack= require('gulp-webpack');
 var config = require('../../config').optimize.js;
 
 /**
@@ -8,6 +9,11 @@ var config = require('../../config').optimize.js;
  */
 gulp.task('optimize:js', function() {
   return gulp.src(config.src)
+  	.pipe(webpack({
+  		output: {
+  			filename: "script.js"
+  		}
+  	}))
     .pipe(uglify(config.options))
     .pipe(gulp.dest(config.dest))
     .pipe(size());
